86a03f123a423796d336a503c10ed079be9d67cb
mengyliu <mengyliu@umich.edu>
2019-01-28 12:41:29 -0500

    Updates

---
+++ precache-manifest.7b96a978a8ead4ad5e25104489bdeeff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/legooo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f5e9c291dd70c486138d",
+    "url": "/legooo/static/js/runtime~main.f5e9c291.js"
+  },
+  {
+    "revision": "c8a26a3dc7379f676800",
+    "url": "/legooo/static/js/main.c8a26a3d.chunk.js"
+  },
+  {
+    "revision": "3cbdb4e26e0b96c34f3b",
+    "url": "/legooo/static/js/1.3cbdb4e2.chunk.js"
+  },
+  {
+    "revision": "c8a26a3dc7379f676800",
+    "url": "/legooo/static/css/main.3edd9211.chunk.css"
+  },
+  {
+    "revision": "d51bc8a563ab1519ed9bf5b87c4fc996",
+    "url": "/legooo/index.html"
+  }
+];
