280cda685f34729002312727cb23bdaebb445328
mengyliu <mengyliu@umich.edu>
2019-01-28 00:21:17 -0500

    disable scroll, changed 3d model

--- src/Three/script.js
+++ src/Three/script.js
@@ -23,7 +23,7 @@ function init() {
     scene.background = new THREE.Color (0xf8f8f8)
 
     //roll-over helpers
-    var rollOverGeo = new THREE.BoxBufferGeometry( 50, 50, 50 );
+    var rollOverGeo = new THREE.BoxBufferGeometry( 50, 58, 50 );
     rollOverMaterial = new THREE.MeshBasicMaterial( { color: 0xff0000, opacity: 0.5, transparent: true } );
     rollOverMesh = new THREE.Mesh( rollOverGeo, rollOverMaterial );
     scene.add( rollOverMesh );
@@ -92,28 +92,28 @@ function onDocumentMouseDown( event ) {
             }
             // create cube
         } else {
-            var url = process.env.PUBLIC_URL + '/3003.stl'
-            loader.load(url, geometry => {
-                var material = new THREE.MeshStandardMaterial({
-                    //"red"
-                      color: "#ff0000",
-                      // specular: CSSToHex(shadeColor(color, -20)),
-                      // shininess: 5,
-                      metalness: 0.4,
-                      roughness: 0.5,
-                    });
-                var voxel = new THREE.Mesh(geometry, material);
-                voxel.rotation.set( -Math.PI / 2 ,0, 0);
-                voxel.scale.set( 3, 3, 3 );
-                voxel.castShadow = true;
-                voxel.receiveShadow = true;
-                voxel.position.copy( intersect.point ).add( intersect.face.normal );
-                voxel.position.divideScalar( 50 ).floor().multiplyScalar( 50 ).addScalar( 25.5 );
-                console.log(voxel.position)
-                scene.add(voxel);
-                objects.push( voxel );
-            })
-
+            // var url = process.env.PUBLIC_URL + '/3003.stl'
+            // loader.load(url, geometry => {
+            //     var material = new THREE.MeshStandardMaterial({
+            //         //"red"
+            //           color: "#ff0000",
+            //           // specular: CSSToHex(shadeColor(color, -20)),
+            //           // shininess: 5,
+            //           metalness: 0.4,
+            //           roughness: 0.5,
+            //         });
+            //     var voxel = new THREE.Mesh(geometry, material);
+            var voxel = createMesh()
+            // voxel.rotation.set( -Math.PI / 2 ,0, 0);
+            voxel.scale.set( 3, 3, 3 );
+            voxel.castShadow = true;
+            voxel.receiveShadow = true;
+            voxel.position.copy( intersect.point ).add( intersect.face.normal );
+            voxel.position.divideScalar( 50 ).floor().multiplyScalar( 50 ).addScalar( 25.5 );
+            console.log(voxel.position)
+            scene.add(voxel);
+            objects.push( voxel );
+            
 
         }
         animate();
@@ -147,6 +147,48 @@ function onDocumentKeyUp( event ) {
 function animate() {
     // requestAnimationFrame( animate );
     renderer.render( scene, camera );
+}
+
+function createMesh() {
+    var base = 17;
+    let width = 17;
+    let height = 20;
+    let depth = 17;
+    let knobSize = 6;
+    var dimensions = new THREE.Vector3(1,1,1);
+    var material = new THREE.MeshStandardMaterial({
+    //"red"
+      color: "#ff0000",
+      // specular: CSSToHex(shadeColor(color, -20)),
+      // shininess: 5,
+      metalness: 0.4,
+      roughness: 0.5,
+    });
+    var brickGeo = new THREE.Geometry();
+    var cubeGeo = new THREE.BoxGeometry( width - 0.1, height - 0.1, depth - 0.1 );
+    var cylinderGeo = new THREE.CylinderGeometry( knobSize, knobSize, knobSize, 20);
+    brickGeo.mergeMesh(new THREE.Mesh(cubeGeo));
+
+    for ( var i = 0; i < dimensions.x; i++ ) {
+        for ( var j = 0; j < dimensions.z; j++ ) {
+            //hah
+          var cylinder = new THREE.Mesh(cylinderGeo, material);
+          cylinder.position.setX(base * i - ((dimensions.x - 1) * base / 2));
+          cylinder.position.setY(base / 1.6);  // TODO to be reworked
+          cylinder.position.setZ(base * j - ((dimensions.z - 1) * base / 2));
+
+          cylinder.castShadow = true;
+          cylinder.receiveShadow = true;
+          brickGeo.mergeMesh( cylinder );
+        }
+    }
+    brickGeo.mergeVertices();
+    brickGeo.castShadow = true;
+    brickGeo.receiveShadow = true;
+    var brick = new THREE.Mesh(brickGeo, material)
+    return brick;
+    
+    
 }
 
 export {init, animate};
